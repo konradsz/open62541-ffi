5e157d4ffaef750f07a5b20491232a47282f444b
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2021-08-03 13:48:24 +0200

    allowlist functions

--- build.rs
+++ build.rs
@@ -1,6 +1,4 @@
-use std::collections::HashSet;
-use std::env;
-use std::path::PathBuf;
+use std::{collections::HashSet, env, path::PathBuf};
 
 #[derive(Debug)]
 struct IgnoreMacros(HashSet<String>);
@@ -16,44 +14,38 @@ impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
 }
 
 fn main() {
-    // Builds the project in the directory located in `libopen62541`, installing it
-    // into $OUT_DIR/lib
-    let dst = cmake::Config::new("open62541")
-        //.define("UA_ENABLE_AMALGAMATION", "On")
-        .build();
+    let dst = cmake::build("open62541");
 
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-lib=open62541");
+    println!("cargo:rerun-if-changed=wrapper.h");
 
-    let ignored_macros = IgnoreMacros(
-        vec![
-            "FP_INFINITE".into(),
-            "FP_NAN".into(),
-            "FP_NORMAL".into(),
-            "FP_SUBNORMAL".into(),
-            "FP_ZERO".into(),
-            "IPPORT_RESERVED".into(),
-        ]
-        .into_iter()
-        .collect(),
-    );
+    // let ignored_macros = IgnoreMacros(
+    //     vec![
+    //         "FP_INFINITE".into(),
+    //         "FP_NAN".into(),
+    //         "FP_NORMAL".into(),
+    //         "FP_SUBNORMAL".into(),
+    //         "FP_ZERO".into(),
+    //         "IPPORT_RESERVED".into(),
+    //     ]
+    //     .into_iter()
+    //     .collect(),
+    // );
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
     let bindings = bindgen::Builder::default()
         .clang_arg(format!("-I{}/include", dst.display()))
-        // The input header we would like to generate
-        // bindings for.
         .header("wrapper.h")
-        .parse_callbacks(Box::new(ignored_macros))
+        //.parse_callbacks(Box::new(ignored_macros))
         .rustfmt_bindings(true)
-        // Finish the builder and generate the bindings.
+        .allowlist_function("UA_Server_new")
+        .allowlist_function("UA_Server_getConfig")
+        .allowlist_function("UA_ServerConfig_setMinimalCustomBuffer")
+        .allowlist_function("UA_Server_run")
+        .allowlist_function("UA_Server_delete")
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
