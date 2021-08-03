810a38f1e81df7b7162dd9102e2fc1a74dd3777d
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2021-08-03 13:35:14 +0200

    add bindings, implement tutorial server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+mod open62541;
+
+use anyhow::Result;
+use signal_hook::iterator::Signals;
+use std::{
+    sync::{atomic::AtomicBool, Arc},
+    thread,
+};
+
+fn main() -> Result<()> {
+    let server = unsafe { open62541::UA_Server_new() };
+    let config = unsafe { open62541::UA_Server_getConfig(server) };
+    unsafe {
+        open62541::UA_ServerConfig_setMinimalCustomBuffer(config, 4840, std::ptr::null(), 0, 0)
+    };
+
+    let mut signals = Signals::new(&[signal_hook::consts::SIGINT, signal_hook::consts::SIGTERM])?;
+
+    let running = Arc::new(AtomicBool::new(true));
+    let running_clone = running.clone();
+    thread::spawn(move || {
+        if let Some(_) = signals.into_iter().next() {
+            running_clone.store(false, std::sync::atomic::Ordering::Relaxed);
+        }
+    });
+    let running = Arc::<AtomicBool>::as_ptr(&running).cast();
+
+    let retval = unsafe { open62541::UA_Server_run(server, running) };
+    unsafe { open62541::UA_Server_delete(server) };
+
+    println!("Retval: {}", retval);
+
+    Ok(())
 }
