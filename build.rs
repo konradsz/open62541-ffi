97807063a60f67dd5e30f363a7cd4f85b10d19a6
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2021-08-03 13:29:32 +0200

    add submodule and build.rs

---
+++ build.rs
@@ -0,0 +1,63 @@
+use std::collections::HashSet;
+use std::env;
+use std::path::PathBuf;
+
+#[derive(Debug)]
+struct IgnoreMacros(HashSet<String>);
+
+impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
+    fn will_parse_macro(&self, name: &str) -> bindgen::callbacks::MacroParsingBehavior {
+        if self.0.contains(name) {
+            bindgen::callbacks::MacroParsingBehavior::Ignore
+        } else {
+            bindgen::callbacks::MacroParsingBehavior::Default
+        }
+    }
+}
+
+fn main() {
+    // Builds the project in the directory located in `libopen62541`, installing it
+    // into $OUT_DIR/lib
+    let dst = cmake::Config::new("open62541")
+        //.define("UA_ENABLE_AMALGAMATION", "On")
+        .build();
+
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-lib=open62541");
+
+    let ignored_macros = IgnoreMacros(
+        vec![
+            "FP_INFINITE".into(),
+            "FP_NAN".into(),
+            "FP_NORMAL".into(),
+            "FP_SUBNORMAL".into(),
+            "FP_ZERO".into(),
+            "IPPORT_RESERVED".into(),
+        ]
+        .into_iter()
+        .collect(),
+    );
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        .clang_arg(format!("-I{}/include", dst.display()))
+        // The input header we would like to generate
+        // bindings for.
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(ignored_macros))
+        .rustfmt_bindings(true)
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    println!("Wrote {}/bindings.rs", env::var("OUT_DIR").unwrap());
+}
---
+++ wrapper.h
@@ -0,0 +1,2 @@
+#include <open62541/server.h>
+#include <open62541/server_config_default.h>
